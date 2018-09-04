b7a41e17f25363feff31e87ea256992665788cb1
yein <trie60@gmail.com>
2018-09-04 18:47:35 -0400

    add link to world1
    add link to world1

--- worlds/index/index.js
+++ worlds/index/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
     constructor(props) {
@@ -10,6 +11,7 @@ class Home extends React.Component {
         return(
             <div>
             this is my landing page
+            <li><Link to='/world1'>to the world1</Link></li>
             </div>
         )
     }
